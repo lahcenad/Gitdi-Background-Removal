de35daa7e074d31ecd1d9d547e9c6401708dedda
Lahcen adeghoui <54738559+lahcenad@users.noreply.github.com>
2023-10-25 11:36:08 +0100

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -6,9 +6,9 @@ import base64
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
+st.write("## Gitdi - Remove background from your image")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    "uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar."
 )
 st.sidebar.write("## Upload and download :gear:")
 
